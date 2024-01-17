44421a3c18f6b489c26288b012b08bc5ae755bdf
Sam Sulaimanov <sam@octanis.ch>
2024-01-17 19:44:27 +0100

    updating readme

--- firmware/synthed_net/memenet/sampleoutput.h
+++
@@ -1,6 +0,0 @@
-// This file was @generated automatically
-
-#define SAMPLE_OUTPUT { \
-  0x50408000, 0x000000ff, 0x00000001, 0x00000000, 0x50400000, 0xffffffff, 0x00000001, 0x7f7f777f, \
-  0x00000000 \
-}
